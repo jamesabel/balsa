cd55c095b706fa3919d8d55e3d3b47792514d42d
james abel <j@abel.co>
2018-06-08 18:08:40 -0700

    refactor sentry support add sentry test

--- balsa/balsa.py
+++ balsa/balsa.py
@@ -75,22 +75,29 @@ class DialogBoxHandler(logging.NullHandler):
 
 @attrs
 class Balsa(object):
+
+    # commonly used options
     name = attrib()
     author = attrib()
     verbose = attrib(default=False)
     gui = attrib(default=False)
     delete_existing_log_files = attrib(default=False)
+
     max_bytes = attrib(default=100*1E6)
     backup_count = attrib(default=3)
-    sentry = attrib(default=False)
-    sentry_dsn = attrib(default=None)
     error_callback = attrib(default=None)
     log_directory = attrib(default=None)
     log_path = attrib(default=None)
     log_formatter = attrib(default=logging.Formatter('%(asctime)s - %(name)s - %(filename)s - %(lineno)s - %(funcName)s - %(levelname)s - %(message)s'))
     handlers = attrib(default=None)
     root_log = attrib(default=None)
-    inhibit_cloud_services = attrib(default=False)  # True to inhibit messages from going to cloud services
+
+    # cloud services
+    # set inhibit_cloud_services to True to inhibit messages from going to cloud services (good for testing)
+    inhibit_cloud_services = attrib(default=False)
+    sentry_client = attrib(default=None)
+    sentry = attrib(default=False)
+    sentry_dsn = attrib(default=None)
 
     def init_logger_from_args(self, args):
         """
@@ -170,18 +177,18 @@ class Balsa(object):
         # For the Client to work you need a SENTRY_DSN environmental variable set, or one must be provided
         # Set self.test_mode to True to inhibit Sentry messages
         if self.sentry:
-            if self.sentry_dsn:
-                client = raven.Client(
-                    dsn=self.sentry_dsn,
-                    sample_rate=0.0 if self.inhibit_cloud_services else 1.0,
-                )
-            else:
-                client = raven.Client(
+            if self.sentry_dsn is None:
+                self.sentry_client = raven.Client(
                     dsn=os.environ['SENTRY_DSN'],
                     sample_rate=0.0 if self.inhibit_cloud_services else 1.0,
                 )
+            else:
+                self.sentry_client = raven.Client(
+                    dsn=self.sentry_dsn,
+                    sample_rate=0.0 if self.inhibit_cloud_services else 1.0,
+                )
 
-            sentry_handler = SentryHandler(client)
+            sentry_handler = SentryHandler(self.sentry_client)
             sentry_handler.setLevel(logging.ERROR)
 
             self.root_log.addHandler(sentry_handler)
---
+++ test_balsa/test_sentry.py
@@ -0,0 +1,21 @@
+
+import os
+
+from balsa import get_logger, Balsa, __author__
+
+
+def test_balsa_sentry():
+    application_name = 'test_balsa_sentry'
+
+    if 'SENTRY_DSN' in os.environ:
+        balsa = Balsa(application_name, __author__, sentry=True, inhibit_cloud_services=False)
+        balsa.init_logger()
+
+        log = get_logger(application_name)
+        log.error('test balsa sentry error message')
+    else:
+        print('Please set SENTRY_DSN environment variable to have a good %s test' % __name__)
+
+
+if __name__ == '__main__':
+    test_balsa_sentry()
