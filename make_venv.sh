136ec00ad17ee3a84bda1a3f1aa01107f8c065a3
james abel <j@abel.co>
2018-10-04 18:47:26 -0700

    use requirements-dev.txt

--- make_venv.sh
+++ make_venv.sh
@@ -2,4 +2,4 @@
 python3 -m venv --clear venv
 venv/bin/pip install -U pip --upgrade pip
 venv/bin/pip install -U setuptools
-venv/bin/pip install -U -r requirements.txt
+venv/bin/pip install -U -r requirements-dev.txt
