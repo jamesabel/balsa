3726759320297807b762fb9326ace66516912da1
james abel <j@abel.co>
2019-09-11 16:26:58 -0700

    Python 3.7 (from 3.6)

--- make_venv.bat
+++ make_venv.bat
@@ -1,7 +1,7 @@
 REM set PYTHON_EXE environment variable before running if a different python is desired
 if DEFINED PYTHON_EXE goto RUNVENV
 REM default
-set PYTHON_EXE="\Program Files\Python36\python.exe"
+set PYTHON_EXE="\Program Files\Python37\python.exe"
 :RUNVENV
 %PYTHON_EXE% -m venv --clear venv
 venv\Scripts\python.exe -m pip install --no-deps --upgrade pip
