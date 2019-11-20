ebba12d214372b3e4fc388f03fa5782281d1706e
James Abel <j@abel.co>
2019-11-20 13:40:06 -0800

    migrate to twine

--- pypi.bat
+++ pypi.bat
@@ -4,4 +4,5 @@ del /Q dist\*.*
 copy /Y LICENSE LICENSE.txt
 copy /Y docs\source\readme.rst readme.rst
 copy /Y docs\source\readme.rst balsa\readme.rst
-venv\Scripts\python.exe setup.py bdist_wheel upload -r pypi
+venv\Scripts\python.exe setup.py bdist_wheel
+twine upload dist/*
