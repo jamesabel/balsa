cae8c2508ca0fec13539d832f3beeed111075a84
James Abel <j@abel.co>
2020-10-14 16:47:00 -0700

    upgrade to Python 3.8

--- make_venv.bat
+++ make_venv.bat
@@ -1,9 +1,5 @@
-REM set PYTHON_EXE environment variable before running if a different python is desired
-if DEFINED PYTHON_EXE goto RUNVENV
-REM default
-set PYTHON_EXE="\Program Files\Python37\python.exe"
-:RUNVENV
-%PYTHON_EXE% -m venv --clear venv
+rmdir /S /Q venv
+C:"\Program Files\Python38\python.exe" -m venv --clear venv
 venv\Scripts\python.exe -m pip install --no-deps --upgrade pip
 venv\Scripts\pip3 install -U setuptools
 venv\Scripts\pip3 install -U -r requirements-dev.txt
--- pypi.bat
+++ pypi.bat
@@ -5,4 +5,4 @@ copy /Y LICENSE LICENSE.txt
 copy /Y docs\source\readme.rst readme.rst
 copy /Y docs\source\readme.rst balsa\readme.rst
 venv\Scripts\python.exe setup.py bdist_wheel
-twine upload dist/*
+venv\Scripts\twine upload dist/*
