cd7e1f950fab390df1cdae68b6ba6ce3c6ad252d
JamesAbel <j@abel.co>
2025-01-02 09:53:18 -0800

    improve the demo structure

--- examples/balsa_gui_example.py
+++ examples/balsa_gui_example.py
@@ -1,16 +1,25 @@
-
+from PyQt5.QtWidgets import QApplication, QMessageBox
 from balsa import get_logger, Balsa
-from example import application_name, author, something_useful
+
+application_name = "balsa_gui_example"
+author = "balsa"
 
 log = get_logger(name=application_name)
 
-
 def main():
+    app = QApplication([])
+
     balsa = Balsa(application_name, author, gui=True)
     balsa.init_logger()
 
-    something_useful()
+    dialog_box = QMessageBox()
+    dialog_box.setText("Please close this window to end the demo ...")
+    dialog_box.setWindowTitle(application_name)
+    dialog_box.show()
 
+    log.error('I am an error message ...')
+
+    app.exec()
 
 if __name__ == '__main__':
     main()
